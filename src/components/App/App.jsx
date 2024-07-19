e3c8e6d36859aa5987030371b17603b922ea10f9
crystaller347 <qwertymum04@gmail.com>
2024-07-19 13:19:30 +0300

    create components

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,7 +4,11 @@ import css from './App.module.css'
 export default function App() {
 
   return (
-    <>
-    </>
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <SearchBox />
+      <ContactList />
+    </div>
   )
 }
---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,1 @@
+import css from './ContactList.module.css'
